73db0aebbfb2152780082e24848d6ccb1af9855b
Tianyu K <isscaorz@gmail.com>
2018-02-28 17:58:52 +0800

    a

--- read.js
+++ read.js
@@ -2,6 +2,9 @@
  * jQuery JavaScript Library v1.11.3
  * http://jquery.com/
  * 解析开始时间：2018-01-20
+ * 解析参考：
+ * http://rapheal.sinaapp.com/tag/jquery-2/
+ * 
  * 
  * 符号预定义： 
  * //------------------------------------------------------------------------
@@ -32,23 +35,94 @@
 	var class2type = {}; //定义一个空对象 {}，储存toString和hasOwnProperty方法
 	var toString = class2type.toString; //将toString方法储存到class2type
 	var hasOwn = class2type.hasOwnProperty; //将hasOwnProperty方法储存到class2type并缩写为hasOwn
-	var support = {};
-	var version = "1.11.3",
-		// Define a local copy of jQuery
-		jQuery = function(selector, context) {
-			// The jQuery object is actually just the init constructor 'enhanced'
-			// Need init if jQuery is called (just allow error to be thrown if not included)
-			return new jQuery.fn.init(selector, context);
+	var support = {}; //???
+	var version = "1.11.3", //定义当前版本号
+		jQuery = function(selector, context) { //实例化jQuery对象 ,selector是选择器，context是上下文			
+			return new jQuery.fn.init(selector, context); //jQuery对象实际上只是init构造函数增强,如果调用jQuery就需要初始化（如果不包括，则允许抛出错误）
 		},
-		// Support: Android<4.1, IE<9
-		// Make sure we trim BOM and NBSP
-		rtrim = /^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g,
-		// Matches dashed string for camelizing
-		rmsPrefix = /^-ms-/,
-		rdashAlpha = /-([\da-z])/gi,
-		// Used by jQuery.camelCase as callback to replace()
-		fcamelCase = function(all, letter) {
-			return letter.toUpperCase();
+		rtrim = /^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g, //确保去除BOM(字节顺序标记)和 &nbsp,支持Android<4.1, IE<9。
+		rmsPrefix = /^-ms-/, //匹配用于驼峰命名化的虚线，IE中的前缀 -ms 转成： Ms  
+		rdashAlpha = /-([\da-z])/gi, //匹配用于驼峰命名化的虚线，转大小写-left转成 Left
+		fcamelCase = function(all, letter) { //被jQuery.camelCase用作回调函数来replace()
+			return letter.toUpperCase(); //字符串letter转换为大写
 		};
+
+	jQuery.fn = jQuery.prototype = {//给jQuery.prototype设置别名jQuery.fn
+		jquery: version, //当前使用的jQuery版本
+		constructor: jQuery, //手动让jQuery.prototype.constructor指回 jQuery
+		selector: "", //定义一个空的选择器selector
+		length: 0,//jQuery对象的长度，默认为 0
+		toArray: function() {//将jQuery对象转换成数组类型
+			return slice.call(this); //[].slice.call(this), 相当于Array.prototype.slice.call(this)
+		},
+
+		// Get the Nth element in the matched element set OR
+		// Get the whole matched element set as a clean array
+		get: function(num) {
+			return num != null ?
+
+				// Return just the one element from the set
+				(num < 0 ? this[num + this.length] : this[num]) :
+
+				// Return all the elements in a clean array
+				slice.call(this);
+		},
+
+		// Take an array of elements and push it onto the stack
+		// (returning the new matched element set)
+		pushStack: function(elems) {
+
+			// Build a new jQuery matched element set
+			var ret = jQuery.merge(this.constructor(), elems);
+
+			// Add the old object onto the stack (as a reference)
+			ret.prevObject = this;
+			ret.context = this.context;
+
+			// Return the newly-formed element set
+			return ret;
+		},
+
+		// Execute a callback for every element in the matched set.
+		// (You can seed the arguments with an array of args, but this is
+		// only used internally.)
+		each: function(callback, args) {
+			return jQuery.each(this, callback, args);
+		},
+
+		map: function(callback) {
+			return this.pushStack(jQuery.map(this, function(elem, i) {
+				return callback.call(elem, i, elem);
+			}));
+		},
+
+		slice: function() {
+			return this.pushStack(slice.apply(this, arguments));
+		},
+
+		first: function() {
+			return this.eq(0);
+		},
+
+		last: function() {
+			return this.eq(-1);
+		},
+
+		eq: function(i) {
+			var len = this.length,
+				j = +i + (i < 0 ? len : 0);
+			return this.pushStack(j >= 0 && j < len ? [this[j]] : []);
+		},
+
+		end: function() {
+			return this.prevObject || this.constructor(null);
+		},
+
+		// For internal use only.
+		// Behaves like an Array's method, not like a jQuery method.
+		push: push,
+		sort: deletedIds.sort,
+		splice: deletedIds.splice
+	};
 
 }));
--- read_txt.js
+++ read_txt.js
@@ -22,18 +22,104 @@
 		factory(global);
 	}
 }(typeof window !== "undefined" ? window : this, function(window, noGlobal) {
-	//具体的库函数factory见下文	
+	//------------------------------------------------------------------------
+	//具体的库函数factory如下
+	//------------------------------------------------------------------------
+	/*
+	 * deletedIds = [] //定义一个空数组对象deletedIds，储存slice、concat、push、indexOf四个方法入口
+	 * class2type = {} //定义一个空对象class2type，储存toString和hasOwnProperty方法
+	 */
+	var deletedIds = [];
+	var slice = deletedIds.slice;
+	var concat = deletedIds.concat;
+	var push = deletedIds.push;
+	var indexOf = deletedIds.indexOf;
+	var class2type = {};
+	var toString = class2type.toString;
+	var hasOwn = class2type.hasOwnProperty;
+	var support = {}; //???
+	/*
+	 * version是当前版本号
+	 * jQuery对象实际上只是init构造函数增强,如果调用jQuery就需要初始化（如果不包括，则允许抛出错误）
+	 * --jQuery没有使用new运算符将jQuery显示的实例化，而是直接调用其函数
+	 * --要实现这样,那么jQuery就要看成一个类，且返回一个正确的实例，实例还要能正确访问jQuery类原型上的属性与方法
+	 * --通过原型传递(jQuery.fn.init.prototype = jQuery.fn)解决问题，把jQuery的原型传递给jQuery.prototype.init.prototype 
+	 * --通过这个方法生成的实例this所指向的仍然是jQuery.fn(jQuery.prototype)，能正确访问 jQuery类原型上的属性与方法
+	 * rtrim用来确保去除BOM和 &nbsp,支持Android<4.1, IE<9。
+	 * --BOM为byte-order mark的缩写，意思是字节顺序标记(https://zh.wikipedia.org/wiki/%E4%BD%8D%E5%85%83%E7%B5%84%E9%A0%86%E5%BA%8F%E8%A8%98%E8%99%9F)
+	 * rmsPrefix和rdashAlpha: 匹配用于驼峰命名化的虚线
+	 * --rmsPrefix: IE中的前缀 -ms 转成  Ms
+	 * --rdashAlpha: 转大小写 -left转成  Left
+	 * fcamelCase: 被jQuery.camelCase用作回调函数来replace()
+	 * --返回第二个参数letter转为大写的值
+	 */
+	var version = "1.11.3",
+		jQuery = function(selector, context) {
+			return new jQuery.fn.init(selector, context);
+		},
+		rtrim = /^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g,
+		rmsPrefix = /^-ms-/,
+		rdashAlpha = /-([\da-z])/gi,
+		fcamelCase = function(all, letter) {
+			return letter.toUpperCase();
+		};
+	/*
+	 * jQuery.fn为jQuery的原型(jQuery.prototype)的别名, 挂载在jQuery.prototype上的方法可让所有jQuery对象使用
+	 * jQuery.fn.jquery: 当前使用的jQuery版本
+	 * jQuery.fn.constructor: 构造函数，手动让jQuery.prototype.constructor指回 jQuery
+	 * --由于采用对象字面量的方式 jQuery.prototype = {} 重写了 jQuery.prototype,所以jQuery.prototype.constructor 将指向 Object
+	 * jQuery.fn.selector: 定义一个空的选择器selector
+	 * jQuery.fn.length: jQuery对象的长度，默认为 0。也即是jQuery对象里边选取的DOM节点数目。
+	 * jQuery.fn.toArray: 将jQuery对象转换成数组类型，相当于Array.prototype.slice.call(this)
+	 * 
+	 * 
+	 */
+	jQuery.fn = jQuery.prototype = {
+		jquery: version,
+		constructor: jQuery,
+		selector: "",
+		length: 0,
+		toArray: function() {
+			return slice.call(this);
+		},
+		get: function(num) {
+			return num != null ?
+				(num < 0 ? this[num + this.length] : this[num]) :
+				slice.call(this);
+		},
+		pushStack: function(elems) {
+			var ret = jQuery.merge(this.constructor(), elems);
+			ret.prevObject = this;
+			ret.context = this.context;
+			return ret;
+		},
+		each: function(callback, args) {
+			return jQuery.each(this, callback, args);
+		},
+		map: function(callback) {
+			return this.pushStack(jQuery.map(this, function(elem, i) {
+				return callback.call(elem, i, elem);
+			}));
+		},
+		slice: function() {
+			return this.pushStack(slice.apply(this, arguments));
+		},
+		first: function() {
+			return this.eq(0);
+		},
+		last: function() {
+			return this.eq(-1);
+		},
+		eq: function(i) {
+			var len = this.length,
+				j = +i + (i < 0 ? len : 0);
+			return this.pushStack(j >= 0 && j < len ? [this[j]] : []);
+		},
+		end: function() {
+			return this.prevObject || this.constructor(null);
+		},
+		push: push,
+		sort: deletedIds.sort,
+		splice: deletedIds.splice
+	};
 }));
-//------------------------------------------------------------------------
-//具体的库函数factory
-//------------------------------------------------------------------------
-
-var deletedIds = [];
-var slice = deletedIds.slice;
-var concat = deletedIds.concat;
-var push = deletedIds.push;
-var indexOf = deletedIds.indexOf;
-var class2type = {};
-var toString = class2type.toString;
-var hasOwn = class2type.hasOwnProperty;
-var support = {};
